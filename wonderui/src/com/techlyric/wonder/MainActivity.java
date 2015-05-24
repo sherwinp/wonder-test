67b429a6c7538d55080983d59b50bc031788a1fd
Sherwin Price <sherwin.price@niksoft.com>
2015-05-24 17:42:08 -0400

    update.

--- wonderui/src/com/techlyric/wonder/MainActivity.java
+++ wonderui/src/com/techlyric/wonder/MainActivity.java
@@ -5,12 +5,12 @@ import android.os.*;
 import android.view.*;
 import android.widget.*;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-	{
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
